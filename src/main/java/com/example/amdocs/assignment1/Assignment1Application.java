c657245ad9165707852c6f66c92464e5e8dddb7b
Demo-Sonarqube <sambhav.184Ca068@nitk.edu.in>
2021-04-15 10:54:48 +0530

    Update Assignment1Application.java

--- src/main/java/com/example/amdocs/assignment1/Assignment1Application.java
+++ src/main/java/com/example/amdocs/assignment1/Assignment1Application.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Vector;
-import java.util.Itertor;
+import java.util.Iterator;
 
 
 import org.springframework.boot.SpringApplication;
