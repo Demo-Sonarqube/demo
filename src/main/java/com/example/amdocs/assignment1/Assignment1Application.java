11f291facd797490bcd8135cb2e0cd176507613e
sambhav25-NITK <sambhavkhandelwal25@gmail.com>
2021-04-15 10:33:55 +0530

    first commit

---
+++ src/main/java/com/example/amdocs/assignment1/Assignment1Application.java
@@ -0,0 +1,54 @@
+package com.example.amdocs.assignment1;
+
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
+import javax.json.stream.JsonParser;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1Application.class, args);
+		
+		
+		readMethodJson();
+	}
+	
+	public static void readMethodJson()
+	{
+		
+		JsonParser parser=Json.createParser(Assignment1Application.class.getResourceAsStream("/jsons.json"));
+		
+		while(parser.hasNext())
+		{
+			JsonParser.Event event= parser.next();
+			System.out.println(event.toString());
+			
+//			 switch (event) {
+//             case KEY_NAME:
+//             case VALUE_STRING:
+//             case VALUE_NUMBER:
+//                 System.out.print(": " + parser.getString());
+//                 break;
+//             case VALUE_NULL:
+//                 System.out.print(": null");
+//                 break;
+//             case VALUE_TRUE:
+//                 System.out.print(": true");
+//                 break;
+//             case VALUE_FALSE:
+//                 System.out.print(": false");
+//         }
+
+         System.out.println();
+		}
+		
+	}
+	
+
+}
