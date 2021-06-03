59b84fd3a74638c64a3ad26a5fd0b930a07555ac
Sonu1311997 <sonusitole195@gmail.com>
2021-06-03 22:33:56 +0530

    Update Sonu.java

--- src/main/java/com/example/amdocs/assignment1/Sonu.java
+++ src/main/java/com/example/amdocs/assignment1/Sonu.java
@@ -1,4 +1,14 @@
-class Sonu{
-  System.out.println("WELCOME SONU!!!!");
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+class Test{
+void abc()
+{
+  Map<String,String> mp=new HashMap<>();
+  mp.put("Sonu","Sitole");
   
+  Set<String> s=new HashSet<>();
+}
 }
